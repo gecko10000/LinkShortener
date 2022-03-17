4cb79ad84e97a7dda5bb0c655e13b45f2312f110
gecko10000 <levtey@gmail.com>
2022-03-17 23:28:32 +0000

    Working without frontend or database

---
+++ Controllers/LinkController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace LinkShortener.Controllers;
+
+[ApiController]
+[Route("")]
+public class LinkController : ControllerBase
+{
+
+  private static Dictionary<string, string> Links = new Dictionary<string, string>();
+
+  [HttpPost("set")]
+  public IActionResult Shorten(string link)
+  {
+    string shortened = GenerateRandomString(link, 4);
+    Links.Add(shortened, link);
+    return Ok(shortened);
+  }
+
+  private string GenerateRandomString(string s, int length)
+  {
+    StringBuilder builder;
+    Random r = new Random(s.GetHashCode());
+    do
+    {
+      builder = new StringBuilder(length);
+      for (int i = 0; i < length; i++)
+      {
+        builder.Append(RandomChar(r));
+      }
+    } while (Links.ContainsKey(builder.ToString()));
+    return builder.ToString();
+  }
+
+  private static string chars = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789";
+
+  private char RandomChar(Random r)
+  {
+    return chars[r.Next(chars.Length)];
+  }
+
+  [HttpGet("{link}")]
+  public IActionResult ProcessLink(string link)
+  {
+    return Links.TryGetValue(link, out string? v) ? Redirect(v) : BadRequest("This link does not exist.");
+  }
+}
