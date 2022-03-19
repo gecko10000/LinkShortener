0453c4d9f68ba04ff198ee6bd6bac5c0fbf7e69e
gecko10000 <levtey@gmail.com>
2022-03-19 02:15:52 +0000

    Implement SQLite database

--- Controllers/LinkController.cs
+++ Controllers/LinkController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
+using System.Data.SQLite;
 
 namespace LinkShortener.Controllers;
 
@@ -10,15 +11,61 @@ public class LinkController : ControllerBase
 
   private static Dictionary<string, string> Links = new Dictionary<string, string>();
 
+  private SQLiteConnection openConnection()
+  {
+    new DirectoryInfo("data").Create();
+    var connection = new SQLiteConnection(@"URI=file:data/links.db");
+    connection.Open();
+    using var command = new SQLiteCommand(connection);
+    command.CommandText = @"CREATE TABLE IF NOT EXISTS links (long TEXT UNIQUE, short TEXT UNIQUE);";
+    command.ExecuteNonQuery();
+    return connection;
+  }
+
+  private string? GetExisting(SQLiteConnection c, string l)
+  {
+    using var command = new SQLiteCommand(c);
+    command.CommandText = @"SELECT short FROM links WHERE long=@long;";
+    command.Parameters.AddWithValue("@long", l);
+    return (string?) command.ExecuteScalar();
+  }
+
+  private bool CheckExists(SQLiteConnection c, string s)
+  {
+    using var command = new SQLiteCommand(c);
+    command.CommandText = @"SELECT long FROM links WHERE short=@short;";
+    command.Parameters.AddWithValue("@short", s);
+    return command.ExecuteScalar() is not null;
+  }
+
+  private void Insert(SQLiteConnection c, string l, string s)
+  {
+    using var command = new SQLiteCommand(c);
+    command.CommandText = @"INSERT INTO links(long, short) VALUES(@long, @short);";
+    command.Parameters.AddWithValue("@long", l);
+    command.Parameters.AddWithValue("@short", s);
+    command.ExecuteNonQuery();
+  }
+
   [HttpPost("set")]
   public IActionResult Shorten([FromForm] string link)
   {
-    string shortened = GenerateRandomString(4);
-    Links.Add(shortened, link);
+    using var connection = openConnection();
+    using var command = new SQLiteCommand(connection);
+
+    // Get existing first
+    string? existing = GetExisting(connection, link);
+    if (existing is not null)
+    {
+      return Ok(existing);
+    }
+
+    string shortened = GenerateRandomString(4, connection);
+    Insert(connection, link, shortened);
     return Ok(shortened);
   }
 
-  private string GenerateRandomString(int length)
+  private string GenerateRandomString(int length, SQLiteConnection connection)
   {
     StringBuilder builder;
     Random r = new Random();
@@ -29,7 +76,7 @@ public class LinkController : ControllerBase
       {
         builder.Append(RandomChar(r));
       }
-    } while (Links.ContainsKey(builder.ToString()));
+    } while (CheckExists(connection, builder.ToString()));
     return builder.ToString();
   }
 
--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+
 app.UseAuthorization();
 
 app.MapControllers();
